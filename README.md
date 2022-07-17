bec1edd8198787099d08ae5021e7cc568f80ba55
Naiyer Asif <19614213+naiyerasif@users.noreply.github.com>
2022-07-17 12:28:59 +0530

    feat(plugin): csso

---
+++ README.md
@@ -0,0 +1,11 @@
+# Lume plugins
+
+Internal Lume plugins used by [mflash.dev](https://mflash.dev)
+
+## [CSSO](./csso/)
+
+Minifies CSS files
+
+## License
+
+Licensed under [MIT](./LICENSE.md)
---
+++ csso/mod.ts
@@ -0,0 +1,22 @@
+import { Page } from 'lume/core/filesystem.ts'
+import { minify } from 'https://unpkg.com/csso@5.0.3/dist/csso.esm.js'
+
+import type { Site } from 'lume/core.ts'
+
+/** A plugin to minify all CSS files */
+export default function () {
+	return (site: Site) => {
+		const options = {
+			extensions: ['.css']
+		}
+
+		site.loadAssets(options.extensions)
+		site.process(options.extensions, minifyStyles)
+
+		function minifyStyles(file: Page) {
+			if (typeof file.content === 'string') {
+				file.content = minify(file.content).css
+			}
+		}
+	}
+}
